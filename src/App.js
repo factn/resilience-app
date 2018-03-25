1af400f034cc5533bbc49153284ba24772cf4b39
danieljauch <daniel.jauch@rabidtech.co.nz>
2018-03-26 11:21:16 +1300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Module imports
+import React, { Component } from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
+// Styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+      </div>
+    );
+  }
+}
+
+export default App;
