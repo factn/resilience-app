6f77ca3ccbd97aaa6cea27176644fba02111ddca
danieljauch <daniel.jauch@rabidtech.co.nz>
2018-03-29 16:01:10 +1300

    Styling funding progress bar

--- src/js/AdModalContent.js
+++ src/js/AdModalContent.js
@@ -27,13 +27,15 @@ export default class AdModalContent extends Component {
 		return (
 			<div className="modal-adcontent-wrap">
 				<h3 className="adcontent-header">Help {toFirstCap(requester_firstname)} {verb} {noun}</h3>
+
 				<figure className="adcontent-image-wrap">
 					<img src={image} alt={disaster} className="adcontent-image"/>
 					<figcaption className="adcontent-image-caption">
 						<p>{disaster}</p>
 						<div className="funding-progress-wrap">
-							<label className="funding-progress-label">Funding goal: {donated} / {funding_goal}</label>
+							<label className="funding-progress-label">Funding goal: {donated} / ${funding_goal}</label>
 							<input type="range"
+									className="funding-progress-slider"
 									id={`${disaster}_fundingGoal_for${toFirstCap(requester_firstname)}`}
 									min={0}
 									max={funding_goal}
@@ -42,6 +44,7 @@ export default class AdModalContent extends Component {
 						</div>
 					</figcaption>
 				</figure>
+
 				<MiniMap initialCenter={{ lat: requestorlat, lng: requestorlon }} />
 			</div>
 		);
