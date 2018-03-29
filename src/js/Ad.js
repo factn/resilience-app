df5a08ebdfe835961dc94091189028c74365caaa
danieljauch <daniel.jauch@rabidtech.co.nz>
2018-03-29 14:19:45 +1300

    Desktop styling complete.

--- src/js/Ad.js
+++ src/js/Ad.js
@@ -41,14 +41,14 @@ export default class Ad extends Component {
 		
 		return (
 			<article className={`ad ${context}-ad`}>
-				<figure className="ad-image-wrap">
-					<img src={imagethumb} alt={disaster} className="ad-image" />
-				</figure>
 				<header className="ad-header">
 					<h4 className="ad-title">{<span>{`Can you ${verb} ${noun} for ${firstName}?`}</span>}</h4>
 					<h5 className="ad-subtitle">{<span>{`${donated} funded`}</span>}</h5>
 				</header>
-				<p className="ad-image-caption">{disaster}</p>
+				<figure className="ad-image-wrap">
+					<img src={imagethumb} alt={disaster} className="ad-image" />
+					<p className="ad-image-caption">{disaster}</p>
+				</figure>
 				<button className="btn ad-modal-btn" onClick={() => openModal(context, scenario.attributes)}>{this.callToActionBuild(context)}</button>
 			</article>
 		);
