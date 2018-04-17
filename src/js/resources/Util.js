a87ed7eed9f2fae8e575ca3eeeac85087c21e08a
danieljauch <daniel.jauch@rabidtech.co.nz>
2018-04-18 09:23:34 +1200

    Small changes for server adjustments

--- src/js/resources/Util.js
+++ src/js/resources/Util.js
@@ -1,57 +1,63 @@
 let globalReader
 
 export function toFirstCap(str) {
-	return str ? str.charAt(0).toUpperCase() + str.slice(1) : false
+  return str ? str.charAt(0).toUpperCase() + str.slice(1) : false
 }
+
 export function getUrlPiece() {
-	let currentUrl = window.location.href.split("/")
-	let lastUrlSegment =
-		currentUrl[currentUrl.length - 1] !== ""
-			? currentUrl[currentUrl.length - 1]
-			: currentUrl[currentUrl.length - 2]
-	let allowed = [
-		"donator",
-		"requester",
-		"verifier",
-		"doer",
-		"login",
-		"thanks",
-		"info",
-		"account",
-		"edit-account",
-		"preferences"
-	]
-	if (allowed.indexOf(lastUrlSegment) === -1) return "donator"
-	else return lastUrlSegment
+  let currentUrl = window.location.href.split("/")
+  let lastUrlSegment =
+    currentUrl[currentUrl.length - 1] !== ""
+      ? currentUrl[currentUrl.length - 1]
+      : currentUrl[currentUrl.length - 2]
+  let allowed = [
+    "donator",
+    "requester",
+    "verifier",
+    "doer",
+    "login",
+    "thanks",
+    "info",
+    "account",
+    "edit-account",
+    "preferences"
+  ]
+  if (allowed.indexOf(lastUrlSegment) === -1) return "donator"
+  else return lastUrlSegment
 }
+
 export function valuify(str) {
-	let words = str.split(" ")
-	let ret = []
-	for (let i = 0, l = words.length; i < l; i++) {
-		ret.push(words[i].toLowerCase())
-	}
-	return ret.join("-")
+  let words = str.split(" ")
+  let ret = []
+  for (let i = 0, l = words.length; i < l; i++) {
+    ret.push(words[i].toLowerCase())
+  }
+  return ret.join("-")
 }
+
 export function unvaluify(str) {
-	let words = str.split("-")
-	let ret = []
-	for (let i = 0, l = words.length; i < l; i++) {
-		ret.push(toFirstCap(words[i]))
-	}
-	return ret.join(" ")
+  let words = str.split("-")
+  let ret = []
+  for (let i = 0, l = words.length; i < l; i++) {
+    ret.push(toFirstCap(words[i]))
+  }
+  return ret.join(" ")
 }
+
 export function prepareFileReader(file) {
-	globalReader = new FileReader()
-	globalReader.readAsDataURL(file)
-	globalReader.onload = () => {}
-	globalReader.onerror = error => {
-		console.log("Error uploading file: ", error)
-	}
-	return globalReader
+  globalReader = new FileReader()
+  globalReader.readAsDataURL(file)
+  globalReader.onload = () => {}
+  globalReader.onerror = error => {
+    console.log("Error uploading file: ", error)
+  }
+  return globalReader
 }
+
 export function getBase64() {
-	return globalReader.result
+  return globalReader.result
 }
+
 export function getScenarioIdFromURL(url) {
-	return url.split("/")[1]
+  return url.split("/")[1]
 }
