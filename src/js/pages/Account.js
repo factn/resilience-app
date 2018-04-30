584f467cdc7d00db2d067c389fb2aef51f7a4e52
danieljauch_rabid <dj@tui.localdomain>
2018-05-01 11:59:34 +1200

    creates profile and updates cookie

--- src/js/pages/Account.js
+++ src/js/pages/Account.js
@@ -1,19 +1,69 @@
 /*** IMPORTS ***/
 // Module imports
 import React, { Component } from "react"
+import Cookies from "js-cookie"
 import { Link } from "react-router-dom"
+import createHistory from "history/createBrowserHistory"
 
 // Page elements
 import Header from "../components/Header"
 import Main from "../components/Main"
 import Footer from "../components/Footer"
 
-// Input
+// Inputs
 import Image from "../components/inputs/Image"
+import Submit from "../components/inputs/Submit"
+
+// Local JS Utilities
+import Database from "../resources/Database"
+import { getBase64 } from "../resources/Util"
 /*** [end of imports] ***/
 
+const history = createHistory()
+
 export default class Account extends Component {
+  createProfile = params => {
+    let imageString = getBase64(params.avatar)
+
+    let json = {
+      data: {
+        type: "users",
+        attributes: {
+          firstname: params.firstname,
+          email: params.email,
+          avatar: imageString,
+          password: params.password,
+          password_confirmation: params.password_confirmation
+        }
+      }
+    }
+
+    Database.createUser(json)
+      .then(result => {
+        // console.log("User successfully created:", result)
+        Cookies.set("userId", result.body.data.id)
+        history.push("/profile")
+        window.location = "/profile"
+      })
+      .catch(error => {
+        // console.error("Error creating user:", error)
+      })
+  }
+
   render() {
+    let buttonObj = {
+      labelPhrase: "Create Accout",
+      clas: "footer-btn user-create-btn",
+      onSubmit: this.createProfile,
+      onSubmitParams: {
+        firstname: "firstname",
+        email: "email",
+        password: "password",
+        password_confirmation: "password_confirmation",
+        avatar: "photo"
+      }
+    }
+
     return (
       <div className="page flow-page create-account-page">
         <Header>
@@ -35,7 +85,7 @@ export default class Account extends Component {
               <h3>Name</h3>
             </header>
             <article className="card input-card name-card">
-              <input type="text" placeholder="Enter a name" />
+              <input type="text" placeholder="Enter a name" id="firstname" />
             </article>
           </section>
           <section className="session-settings email-settings">
@@ -43,7 +93,7 @@ export default class Account extends Component {
               <h3>Email</h3>
             </header>
             <article className="card input-card email-card">
-              <input type="email" placeholder="Enter your email" />
+              <input type="email" placeholder="Enter your email" id="email" />
             </article>
           </section>
           <section className="session-settings password-settings">
@@ -51,10 +101,18 @@ export default class Account extends Component {
               <h3>Password</h3>
             </header>
             <article className="card input-card password-card">
-              <input type="password" placeholder="Choose a password" />
+              <input
+                type="password"
+                placeholder="Choose a password"
+                id="password"
+              />
             </article>
             <article className="card input-card password-confirm-card">
-              <input type="password" placeholder="Confirm your password" />
+              <input
+                type="password"
+                placeholder="Confirm your password"
+                id="password_confirmation"
+              />
             </article>
           </section>
           <section className="session-settings">
@@ -67,9 +125,7 @@ export default class Account extends Component {
 
         <Footer>
           <div className="button-label">Create your profile</div>
-          <Link to="/profile" className="btn footer-btn feed-btn">
-            Submit
-          </Link>
+          <Submit {...buttonObj} />
         </Footer>
       </div>
     )
