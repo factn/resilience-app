3a9b9ba54fbc5691b9f6e53c8027e68d3cb9c7fc
danieljauch_rabid <dj@tui.localdomain>
2018-05-04 14:37:55 +1200

    Added stars, made sure it only happens on superfunding

--- src/js/components/Thanks.js
+++ src/js/components/Thanks.js
@@ -1,6 +1,8 @@
 /*** IMPORTS ***/
 // Module imports
 import React, { Component } from "react"
+import Icon from "@fortawesome/react-fontawesome"
+import { faStar } from "@fortawesome/fontawesome-free-solid";
 /*** [end of imports] ***/
 
 export default class Thanks extends Component {
@@ -8,12 +10,19 @@ export default class Thanks extends Component {
     const { open, dismiss } = this.props
     return (
       <section className={open ? "thank-you-modal open" : "thank-you-modal"}>
-      <h2>Thanks!</h2>
-      <h3>You just made a huge difference</h3>
+        <div className="stars">
+          <Icon className="star" icon={faStar} />
+          <Icon className="star" icon={faStar} />
+          <Icon className="star" icon={faStar} />
+          <Icon className="star" icon={faStar} />
+          <Icon className="star" icon={faStar} />
+        </div>
+        <h2>Thanks!</h2>
+        <h3>You just made a huge difference</h3>
         <div className="social-share-area">
           <div className="social-share-label">Tell your friends:</div>
           <div className="add-this-wrap">
-            <div className="addthis_inline_share_toolbox"></div>
+            <div className="addthis_inline_share_toolbox" />
           </div>
         </div>
         <button className="btn-lite dismiss-btn" onClick={() => dismiss()}>
