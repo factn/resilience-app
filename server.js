6fe92168a4b7511e05bf03450c08fab106b18dfa
mat10tng <mat10tng@gmail.com>
2020-03-22 11:25:48 +0100

    making sure we are listen to heroku port setup from server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(9000);
+port = process.env.PORT || 80
+app.listen(port);
