7c0c3f4e8ae2007c99e82a8fd606d69e81275f3e
Terrence <owusuterrence@gmail.com>
2020-03-22 20:21:06 +0000

    Add Button component with test and stories

---
+++ src/app/component/Button/Button.style.js
@@ -0,0 +1,49 @@
+import styled, { css } from 'styled-components';
+import { colors, sizes, iconPositions } from './utils';
+
+// Handles different button styles
+const buttonStyles = {
+  primary: css`
+    background-color: ${colors.primary.background};
+    box-sizing: border-box;
+    border: 1px solid ${colors.primary.background};
+  `,
+  secondary: css`
+    border: 1px solid ${colors.secondary.border};
+    background-color: ${colors.secondary.background};
+  `,
+  tertiary: css`
+    border: 1px solid ${colors.tertiary.border};
+    background-color: ${colors.tertiary.background};
+  `,
+  rounded: css`
+    border-radius: 25px;
+  `
+};
+
+const StyledButton = styled.button`
+  padding: 11px 16px;
+  cursor: pointer;
+  color: #ffffff;
+  outline: none;
+  border-radius: 2px;
+  font-size: 12px;
+  font-weight: bold;
+  line-height: 16px;
+  ${props => sizes[props.size]};
+  &:disabled {
+    border: 1px solid ${colors.disabled.border};
+    color: ${colors.disabled.color};
+    background-color: ${colors.disabled.border};
+    cursor: no-drop;
+  }
+  ${props => props.primary && buttonStyles['primary']};
+  ${props => props.secondary && buttonStyles['secondary']};
+  ${props => props.tertiary && buttonStyles['tertiary']};
+  ${props => props.rounded && buttonStyles['rounded']};
+`;
+
+export const StyledIcon = styled.span`
+  ${props => props.text && iconPositions[props.iconPosition]};
+`;
+export default StyledButton;
---
+++ src/app/component/Button/utils.js
@@ -0,0 +1,34 @@
+export const colors = {
+  primary: {
+    background: '#140D60'
+  },
+  secondary: {
+    background: '#D31409',
+    border: ' #D31409'
+  },
+
+  tertiary: {
+    background: '#F3D3EE'
+  },
+  disabled: {
+    border: '#e5e5e5',
+    color: 'rgba(0, 0, 0, 0.2)'
+  }
+};
+
+export const sizes = {
+  lg: {
+    width: '100%'
+  }
+};
+
+export const iconPositions = {
+  left: {
+    float: 'left',
+    marginRight: '6px'
+  },
+  right: {
+    marginLeft: '6px',
+    float: 'right'
+  }
+};
