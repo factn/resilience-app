623c5326fcb8609dc50d79dbb0bf6a07ec512f16
mat10tng <mat10tng@gmail.com>
2020-03-22 20:37:39 +0100

    add mvp for page component

---
+++ src/app/component/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Text = (text, ...props) => {
+  return <div>hi, this is a fake text component</div>;
+};
+
+export default Text;
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
   },
 });
