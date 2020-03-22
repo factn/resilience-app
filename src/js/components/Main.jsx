733982a58016cf6a8fd4e8299918576232bceebb
mat10tng <mat10tng@gmail.com>
2020-03-22 18:26:01 +0100

    new initial repo

--- src/js/components/Main.jsx
+++
@@ -1,8 +0,0 @@
-/*** IMPORTS ***/
-// Module imports
-import React from "react"
-/*** [end of imports] ***/
-
-const Main = ({ children }) => <div className="app-main">{children}</div>
-
-export default Main
--- src/js/store/Reducer.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import {DonationReducer} from './models/Donation';
-
-export default combineReducers({
-  donation: DonationReducer,
-})
