fac402be445e65c84abcb1094f1fd02a99a52131
Terrence <owusuterrence@gmail.com>
2020-03-22 20:21:06 +0000

    Add Button component with test and stories

--- src/App.js
+++ src/App.js
@@ -2,54 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 
-import 'typeface-roboto';
 import './App.css';
+import Button from './app/component/Button/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <h1>test material ui</h1>
+        <h1>test material ui</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+
+        <Button text="Primary" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <span>
           <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
             React
           </a>
           <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
             Redux
           </a>
           <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
             Redux Toolkit
           </a>
           ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
             React Redux
           </a>
         </span>
