f8b3854d783dd836531227cf939ba59d803dadee
ndurbin <nick.durbin@gmail.com>
2020-03-23 18:33:19 -0400

    Extracted the routing to another component to clean up the file for maintainability.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-import logo from "./logo.svg";
-import "./App.css";
-import theme from "./theme"
+import './App.css';
+import theme from './theme';
 
-import LoginPage from "./app/page/Login";
-import HomePage from "./app/page/Home";
+// Holds all routes with the BrowserRouter, Switch, Route logic
+import Router from './app/router/Router';
 
-const customTheme = createMuiTheme(theme)
+const customTheme = createMuiTheme(theme);
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <ThemeProvider theme={customTheme}>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+        <Router />
       </ThemeProvider>
     </React.Fragment>
   );
