e3a55d800b42751b44cc07f9900cd72393f646d1
mat10tng <mat10tng@gmail.com>
2020-03-23 07:42:35 +0100

    add css baseline from material-ui, this remsemble normalize.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
+import logo from "./logo.svg";
 import "./App.css";
+
 import LoginPage from "./app/page/Login";
 import HomePage from "./app/page/Home";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <HomePage/>
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <React.Fragment>
+      <CssBaseline />
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </React.Fragment>
   );
 }
 
