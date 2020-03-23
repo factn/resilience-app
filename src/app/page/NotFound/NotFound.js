dc46def23de29c7e8d410641f49f68082d0c6267
ndurbin <nick.durbin@gmail.com>
2020-03-23 18:30:14 -0400

    A notfound view for urls hit that do not exist. Can add a 404 with styling and an error report later.

---
+++ src/app/page/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      {/* Renders when no matching url is found */}
+      <h1>Sorry, but that page doesn't exist</h1>
+    </div>
+  );
+};
+
+export default NotFound;
