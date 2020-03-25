43d8a419b5b06358ebba536655fb7c6083673046
Taylor <wwwaaavvveeerrr@gmail.com>
2020-03-25 18:44:30 -0400

    mission styles, still need to add data from firebas to fill in blanks

--- src/app/layout/Card/Card.style.js
+++ src/app/layout/Card/Card.style.js
@@ -11,4 +11,31 @@ export const StyledCard = styled.div`
   background-color: white;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
     0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  img {
+    text-align: center;
+  }
+  h2 {
+    text-align: center;
+  }
+  p {
+    text-align: center;
+  }
+  h5 {
+    padding-bottom: 2%;
+    text-align: center;
+    border-bottom: 1px solid grey;
+    cursor: pointer;
+  }
+  h5:hover {
+    color: grey;
+  }
+  .side-by-side {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0 13%;
+  }
+  h3 {
+    text-align: center;
+  }
 `;
