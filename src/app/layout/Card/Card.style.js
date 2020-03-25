b97fcecfafafd8644b03020cad7b45a43c94713f
mat10tng <mat10tng@gmail.com>
2020-03-25 12:14:36 +0100

    add layout and firebase, now we can see all the requests

---
+++ src/app/layout/Card/Card.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  padding: 0 25px;
+  flex-grow: 1;
+  font-family: Arimo;
+  background-color: #ffefef;
+`;
