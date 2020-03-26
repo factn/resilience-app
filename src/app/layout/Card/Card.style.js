421b2a5addfb081aaca9554975e8df8ddcec766d
mat10tng <mat10tng@gmail.com>
2020-03-26 16:29:57 +0100

    add layout, add profile page

--- src/app/layout/Card/Card.style.js
+++ src/app/layout/Card/Card.style.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+const propsBoxShadow = (props) =>
+  props.flat
+    ? ""
+    : " 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);";
+
+const propsBgColor = (props) =>
+  props.template === "pink" ? props.theme.color.pink : props.theme.color.white;
+
 export const StyledCard = styled.div`
   display: flex;
   flex-direction: column;
@@ -8,34 +16,15 @@ export const StyledCard = styled.div`
   padding: 12px;
   flex-grow: 1;
   font-family: Arimo;
-  background-color: white;
-  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
-    0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  box-shadow: ${propsBoxShadow};
+  background-color: ${propsBgColor};
   img {
     text-align: center;
-  }
-  h2 {
-    text-align: center;
-  }
-  p {
-    text-align: center;
-  }
-  h5 {
-    padding-bottom: 2%;
-    text-align: center;
-    border-bottom: 1px solid grey;
-    cursor: pointer;
-  }
-  h5:hover {
-    color: grey;
   }
   .side-by-side {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     margin: 0 13%;
-  }
-  h3 {
-    text-align: center;
   }
 `;
