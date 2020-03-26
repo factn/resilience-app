fb5630abeeb32192eb1ece6ae670fcc8b4c7ae99
Azmain Amin <mdazmainamin@gmail.com>
2020-03-26 11:55:51 -0400

    feat: redesigned the login page to be consistent with the rest of the app. created a phone authentication form. authenticate using phone number

---
+++ src/app/component/SMSLogin/SMSLogin.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid, Button, Typography, TextField } from "@material-ui/core";
+
+function SMSLogin({ loginWithFacebook, handlePhoneNumberChange, handleSMSLogin }) {
+  return (
+    <>
+      <Grid container spacing={3} justify="center">
+        <Grid item>
+          <Typography component="h2" variant="h6">
+            Please sign in below
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={3} justify="center" direction="column">
+        <Grid item>
+          <Button onClick={loginWithFacebook} color="primary" variant="contained">
+            Sign in with FB
+          </Button>
+        </Grid>
+        <Grid item>
+          <Typography component="p" variant="h5">
+            OR
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Grid container direction="column" spacing={3}>
+            <Grid item>
+              <TextField
+                id="phone-number"
+                label="Phone number"
+                type="text"
+                required
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                onChange={handlePhoneNumberChange}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                id="sms-sign-in"
+                onClick={handleSMSLogin}
+                color="secondary"
+                variant="contained"
+              >
+                Sign in with phone
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid id="recaptcha-container" container spacing={3} justify="center"></Grid>
+    </>
+  );
+}
+
+export default SMSLogin;
+
+SMSLogin.propTypes = {
+  loginWithFacebook: PropTypes.func.isRequired,
+  handlePhonenumberChange: PropTypes.func.isRequired,
+  handleSMSLogin: PropTypes.func.isRequired
+};
