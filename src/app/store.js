355ec7cade7ad2deb7d5f8279142b8bd94f12e33
mat10tng <mat10tng@gmail.com>
2020-03-27 11:35:53 +0100

    add protected route for user

--- src/app/store.js
+++ src/app/store.js
@@ -10,4 +10,8 @@ const rootReducer = combineReducers({
 
 // Create store with reducers and initial state
 const initialState = {};
-export const store = createStore(rootReducer, initialState);
+export const store = createStore(
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
