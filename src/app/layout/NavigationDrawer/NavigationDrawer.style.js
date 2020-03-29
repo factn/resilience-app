3774c2d43d9f9b5f05342c8a7ee10cb745f2724b
Carl Summers <summers.carl@gmail.com>
2020-03-29 12:41:18 -0400

    Add side navigation menu to go to missions and user profile

---
+++ src/app/layout/NavigationDrawer/NavigationDrawer.style.js
@@ -0,0 +1,27 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+  link: {
+    textDecoration: "none",
+    display: "inline",
+    color: "#150E60",
+  },
+  root: {
+    width: "100%",
+    height: "100%",
+  },
+  label: {
+    position: "absolute",
+    left: "12.5%",
+    right: "12.5%",
+    top: "25%",
+    bottom: "25%",
+    color: "#150E60",
+  },
+});
