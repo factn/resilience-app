954705f03bd8dd1ca8bddf2fc9d02be3de473453
mat10tng <mat10tng@gmail.com>
2020-03-31 20:05:50 +0200

    now user can log out

---
+++ src/app/component/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { isLoaded, isEmpty } from "react-redux-firebase";
+
+// @ts-ignore
+
+function PrivateRoute({ children, ...rest }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isLoaded(auth) && !isEmpty(auth) ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
