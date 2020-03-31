942e066f979596cbdef368d66107971b2396813a
mat10tng <mat10tng@gmail.com>
2020-03-31 20:22:11 +0200

    add protected route and remove ununsed imports

--- src/app/component/ProtectedRoute.jsx
+++ src/app/component/ProtectedRoute.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { isLoaded, isEmpty } from "react-redux-firebase";
 
-// @ts-ignore
-
-function PrivateRoute({ children, ...rest }) {
+function ProtectedRoute({ children, ...rest }) {
   const auth = useSelector((state) => state.firebase.auth);
   return (
     <Route
