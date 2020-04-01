854cf4da5c67648d5230c77982de7119ef20543c
Taylor <wwwaaavvveeerrr@gmail.com>
2020-04-01 00:53:23 -0400

    created status page

---
+++ src/app/page/Status/Status.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useFirestore, firestoreConnect } from "react-redux-firebase";
+import { useSelector, connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+// objects
+import { User } from "../../model";
+// styles
+import { Typography, Button } from "@material-ui/core";
+import { Page } from "../../layout";
+import styled from "styled-components";
+
+const StyledHeader = styled(Typography)`
+  margin-top: 12vh;
+  padding: 0 3vw;
+`;
+
+const StyledCopy = styled(Typography)`
+  margin-top: 3vh;
+`;
+
+const StyledButton = styled(Button)`
+  margin-top: 24px;
+  flex-grow: 1;
+  width: 20vw;
+`;
+
+const Status = ({}) => {
+  //function to handel the volunteers status
+  function setStatus(status) {
+    console.log(status);
+  }
+
+  return (
+    <Page>
+      <StyledHeader variant="h1">
+        {" "}
+        Manually set your status so others know what you are doing.{" "}
+      </StyledHeader>
+      <StyledCopy variant="h2"> I am... </StyledCopy>
+      <StyledButton
+        color="primary"
+        size="large"
+        variant="contained"
+        onClick={() => setStatus("OnCall")}
+        disableElevation
+      >
+        On Call
+      </StyledButton>
+      <br />
+      <StyledButton
+        color="primary"
+        size="large"
+        variant="contained"
+        onClick={() => setStatus("Available")}
+        disableElevation
+      >
+        Available
+      </StyledButton>
+      <br />
+      <StyledButton
+        color="primary"
+        size="large"
+        variant="contained"
+        onClick={() => setStatus("Offline")}
+        disableElevation
+      >
+        Offline
+      </StyledButton>
+      <br />
+    </Page>
+  );
+};
+
+export default Status;
