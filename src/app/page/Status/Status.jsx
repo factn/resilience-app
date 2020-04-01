2b2605066a139ce2575bb679c6fdae45682f330d
Taylor <wwwaaavvveeerrr@gmail.com>
2020-04-01 16:16:58 -0400

    users able to set their status

--- src/app/page/Status/Status.jsx
+++ src/app/page/Status/Status.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFirestore, firestoreConnect } from "react-redux-firebase";
 import { useSelector, connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { getFirebase, withFirestore } from "react-redux-firebase";
 // objects
 import { User } from "../../model";
 // styles
@@ -18,16 +19,30 @@ const StyledCopy = styled(Typography)`
   margin-top: 3vh;
 `;
 
+const StyledP = styled(Typography)`
+  margin-top: 3vh;
+`;
+
 const StyledButton = styled(Button)`
   margin-top: 24px;
   flex-grow: 1;
   width: 20vw;
 `;
 
-const Status = ({}) => {
+const Status = ({ firestore }) => {
+  // a happy little message
+  const [Thank, setThank] = useState(false);
+  // find the user
+  const firebase = getFirebase();
+  const user = useSelector((state) => state.firebase.auth);
+
   //function to handel the volunteers status
   function setStatus(status) {
-    console.log(status);
+    const userId = user.uid;
+
+    firestore.collection("users").doc(userId).set({ status: status });
+
+    setThank(`Thank You for your service.\nYour status has been set to ${status}`);
   }
 
   return (
@@ -67,8 +82,9 @@ const Status = ({}) => {
         Offline
       </StyledButton>
       <br />
+      <StyledP>{Thank}</StyledP>
     </Page>
   );
 };
 
-export default Status;
+export default withRouter(withFirestore(Status));
