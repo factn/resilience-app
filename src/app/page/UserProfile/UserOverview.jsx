ed464441b478853297025260f546f8f83dc637d8
mat10tng <mat10tng@gmail.com>
2020-04-08 18:45:05 +0200

    remove all ununsed import

--- src/app/page/UserProfile/UserOverview.jsx
+++ src/app/page/UserProfile/UserOverview.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { H5 } from "../../component";
 import { Grid, Avatar, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { useState } from "react";
 import _ from "lodash";
 
 const useStyles = makeStyles((theme) => ({
