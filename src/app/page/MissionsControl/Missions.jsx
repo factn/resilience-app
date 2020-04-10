025873dd08e1120b8d8df52503f74c1603fb40f7
Richie Humphrey <eihcir0@gmail.com>
2020-04-10 16:02:38 -0700

    Moving MissionCard update work to a different ticket

--- src/app/page/MissionsControl/Missions.jsx
+++ src/app/page/MissionsControl/Missions.jsx
@@ -5,7 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Appbar from "./Appbar";
 import Drawer from "./Drawer";
-import MissionCard from "../../component/MissionCard";
+import MissionCard from "./MissionCard";
 
 const useStyles = makeStyles((theme) => ({
   root: {
