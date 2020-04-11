83eb35e33eb96deb4fa7d743a98d42916c85dc13
mat10tng <mat10tng@gmail.com>
2020-04-11 18:00:35 +0200

    change nzme from missions control to dashboard

--- src/app/page/MissionsCreated/Missions.jsx
+++ src/app/page/MissionsCreated/Missions.jsx
@@ -16,6 +16,7 @@ const MissionsPage = ({ auth, history, ...rest }) => {
   const missions = useSelector((state) => state.firestore.ordered.missionsCreated);
 
   return (
+<<<<<<< HEAD
     <Page title="My Requests">
       <MissionList
         missions={missions}
@@ -24,6 +25,29 @@ const MissionsPage = ({ auth, history, ...rest }) => {
         isLoaded={isLoaded(missions)}
         isEmptyText="You have not created any missions"
       />
+=======
+    <Page template="pink" isLoaded={isLoaded(missions)} isEmpty={isEmpty(missions)}>
+      <StyledHeader variant="h1"> My Requests</StyledHeader>
+
+      {missions?.map((mission) => (
+        <Card key={mission.id}>
+          <MissionCard mission={mission} />
+
+          <Grid container justify="center" alignItems="center">
+            <StyledButton
+              variant="outlined"
+              size="large"
+              color="secondary"
+              onClick={() => {
+                history.push(`/missions/${mission.id}`);
+              }}
+            >
+              Details
+            </StyledButton>
+          </Grid>
+        </Card>
+      ))}
+>>>>>>> change nzme from missions control to dashboard
     </Page>
   );
 };
