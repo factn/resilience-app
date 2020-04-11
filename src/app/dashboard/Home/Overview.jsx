e07923a4d994605104561f51b408295311de1840
mat10tng <mat10tng@gmail.com>
2020-04-11 20:02:17 +0200

    rename folder

--- src/app/dashboard/Home/Overview.jsx
+++ src/app/dashboard/Home/Overview.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import { Mission } from "../../model";
-import { H2 } from "../../component";
-import { color } from "../../../theme";
-import OverviewItem from "./OverviewItem";
+import { H2, H4 } from "../../component";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import AnnouncementIcon from "@material-ui/icons/Announcement";
-import PanToolRoundedIcon from "@material-ui/icons/PanToolRounded";
+
 const useStyles = makeStyles((theme) => ({
   content: {
     flexGrow: 1,
@@ -18,68 +17,43 @@ const useStyles = makeStyles((theme) => ({
   },
   overview: {
     width: "400px",
-    height: "160px",
-  },
-  icon: {
-    fontSize: 40,
+    height: "180px",
   },
 }));
 
+const Item = ({ Icon, Title, Nbr, Link }) => (
+  <Grid container>
+    <Grid item xs={2}>
+      {Icon}
+    </Grid>
+    <Grid item xs={10} container direction="column">
+      <Grid item>
+        <H4>{Title}</H4>
+      </Grid>
+      <Grid item>{Nbr}</Grid>
+      <Grid item>{Link}</Grid>
+    </Grid>
+  </Grid>
+);
 const Overview = ({ missions }) => {
   const classes = useStyles();
   const missionsNotStarted = Mission.filterByStatus(missions, "notStarted");
   const missionsQueued = Mission.filterByStatus(missions, "queued");
 
   return (
-    <Grid container direction="column" spacing={2}>
-      <Grid item style={{ alignSelf: "flex-start" }}>
+    <Grid container>
+      <Grid container>
         <H2>Overview</H2>
       </Grid>
       <Grid item>
-        <Grid container spacing={2}>
-          <Grid item>
-            <Card className={classes.overview}>
-              <OverviewItem
-                icon={
-                  <AnnouncementIcon className={classes.icon} style={{ fill: color.darkPink }} />
-                }
-                title="UNASSIGNED MISSIONS"
-                nbr={missionsNotStarted.length}
-                link="View Unassigned Missions"
-                textColor={color.darkPink}
-              />
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card className={classes.overview}>
-              <OverviewItem
-                icon={
-                  <AnnouncementIcon className={classes.icon} style={{ fill: color.darkOrange }} />
-                }
-                title="QUEUED MISSIONS"
-                nbr={missionsQueued.length}
-                link="View Queued Missions"
-                textColor={color.darkOrange}
-              />
-            </Card>
-          </Grid>
-          <Grid item>
-            <Card className={classes.overview}>
-              <OverviewItem
-                icon={
-                  <PanToolRoundedIcon
-                    className={classes.icon}
-                    style={{ fill: color.vibrantPurple }}
-                  />
-                }
-                title="PENDING VOLUNTEERS"
-                nbr="9" //TODO: Get actual votunteer numbers
-                link="View Pending Volunteers"
-                textColor={color.vibrantPurple}
-              />
-            </Card>
-          </Grid>
-        </Grid>
+        <Card className={classes.overview}>
+          <Item
+            Icon={<AnnouncementIcon />}
+            Title="Unassigned Missions"
+            Nbr="60"
+            Link="View Unassigned Missions"
+          />
+        </Card>
       </Grid>
     </Grid>
   );
