7e71bc86fb1418faca61d331cb73fd55b8c0a82d
Azmain Amin <mdazmainamin@gmail.com>
2020-04-11 13:55:48 -0400

    feat: refactored dashboard ir out of pages and in app

--- src/app/page/index.js
+++ src/app/page/index.js
@@ -3,7 +3,11 @@ import Login from "./Login";
 import MakeRequest from "./MakeRequest";
 import MakeMission from "./MakeMission";
 import Missions from "./Missions";
+<<<<<<< HEAD
 import Dashboard from "../dashboard";
+=======
+import Dashboard from "../Dashboard";
+>>>>>>> feat: refactored dashboard ir out of pages and in app
 import MissionsCreated from "./MissionsCreated";
 import MissionsVolunteered from "./MissionsVolunteered";
 import MissionDetails from "./MissionDetails";
