b03a5b34b94e1dda8aac318d9cfe0331f260e1a9
Miles Thompson <utunga@gmail.com>
2020-04-14 00:32:13 +1200

    Add a schema, fireorm and some repositories

---
+++ src/app/model/FoodBoxes.ts
@@ -0,0 +1,25 @@
+import { CustomRepository, getRepository } from 'fireorm';
+import { BaseRepository } from './BaseRepository'
+import { FoodBox } from './schema';
+
+@CustomRepository(FoodBox)
+class FoodBoxRepository extends BaseRepository<FoodBox> { }
+
+
+/**
+ * Defines the volunteer.
+ *
+ * @version 1.0
+ */
+class FoodBoxes {
+
+  repo() : FoodBoxRepository {
+    return getRepository(FoodBox) as FoodBoxRepository;
+  }
+
+  async all() : Promise<Array<FoodBox>> {
+    return await this.repo().find();
+  }
+}
+
+export default new FoodBoxes();
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 // If you enabled Analytics in your project, add the Firebase SDK for Analytics
 import "firebase/analytics";
 import * as firebase from "firebase/app";
+import * as fireorm from 'fireorm';
 import "firebase/auth";
 import "firebase/firestore";
 import React from "react";
@@ -20,7 +21,8 @@ const rrfConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore();
+const firestore = firebase.firestore();
+fireorm.initialize(firestore);
 
 const rrfProps = {
   firebase,
