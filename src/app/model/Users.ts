1ad56854de2b34de8cdf94bf0d3afa23467090d0
Miles Thompson <utunga@gmail.com>
2020-04-14 19:03:17 +1200

    Get something for @Aquib to be able to get started making the Component

--- src/app/model/Users.ts
+++ src/app/model/Users.ts
@@ -4,6 +4,8 @@ import { get } from "lodash";
 import { BaseRepository } from './BaseRepository'
 import { Mission, MissionStatus } from './schema';
 
+import _ from "lodash";
+
 @CustomRepository(User)
 class UserRepository extends BaseRepository<User> { }
 
@@ -14,6 +16,50 @@ class UserRepository extends BaseRepository<User> { }
  * @version 1.0
  */
 class Users {
+
+  repo() : UserRepository {
+    return getRepository(User) as UserRepository;
+  }
+
+  // FIXME need a method to return 'current user' 
+  // via const user = useSelector((state) => state.firebase.auth);
+
+
+  _allUsers?: User[];
+  async allUsers() {
+    if (this._allUsers===undefined)
+      this._allUsers = await this.repo().find();
+    return this._allUsers;
+  }
+
+   /**
+   * Returns all users matching a given text 
+   * @return {Array.<Use"r>}
+   */
+  async usersMatchingLabel(text:string, limit:number) : Promise<User[]>  {
+    
+      // FIXME: for now pulling ust all the users back
+      //        will have to enable full text search to 
+      //        be able to do more complex queries
+      return _.take(
+                _.sortBy(
+                  _.filter(await this.allUsers(),
+                    user=>this.singleUserMatch(user, text)),
+                  ['profileName']),
+              limit);
+  }
+
+  singleUserMatch(user:User, text:string) : boolean {
+    console.log(user)
+    if (text===undefined)
+      return false;
+    text = text.toLowerCase();
+    if (("" + user.phone).indexOf(text)!=-1)
+      return true;
+    if (("" + user.displayName).toLowerCase().indexOf(text) != -1)
+      return true;
+    return false;
+  }
 
   /**
    * Links a user with a phone number, using an SMS code and a Recaptcha.
