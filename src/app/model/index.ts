5e9c7ded8d493fb5d0d3ffe5f2ae844d4cd09f1c
Azmain Amin <mdazmainamin@gmail.com>
2020-04-15 00:39:32 -0400

    Fix/run prettier (#238)
    * run prettier fix as part of build step
    
    * added prettier pre commit hook
    
    * apparently we dont need to add git add
    
    * remove unsued vars from ProfileImage
    
    * Ran Prettier on these files
    
    * Ran Prettier on these files

--- src/app/model/index.ts
+++ src/app/model/index.ts
@@ -3,11 +3,29 @@ import Missions from "./Missions";
 import Organizations from "./Organizations";
 import FoodBoxes from "./FoodBoxes";
 
-// FIXME: i'm sure there'sa better syntax than this 
-import { Organization, User, Mission, MissionLogEvent, 
-         MissionStatus, MissionFundedStatus, MissionPayableStatus, MissionType } from "./schema";
+// FIXME: i'm sure there'sa better syntax than this
+import {
+  Organization,
+  User,
+  Mission,
+  MissionLogEvent,
+  MissionStatus,
+  MissionFundedStatus,
+  MissionPayableStatus,
+  MissionType,
+} from "./schema";
 
-export { Users, Missions, Organizations, FoodBoxes,  // repos
-         Organization, User, Mission, MissionLogEvent, // tables
-         MissionStatus, MissionFundedStatus, MissionPayableStatus, MissionType //enums
-       };
+export {
+  Users,
+  Missions,
+  Organizations,
+  FoodBoxes, // repos
+  Organization,
+  User,
+  Mission,
+  MissionLogEvent, // tables
+  MissionStatus,
+  MissionFundedStatus,
+  MissionPayableStatus,
+  MissionType, //enums
+};
