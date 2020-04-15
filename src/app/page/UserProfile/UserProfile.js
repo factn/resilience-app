64c8cc36e9d8cef81bd1670ba33529c875dc6380
Taylor <wwwaaavvveeerrr@gmail.com>
2020-04-15 15:11:16 -0400

    made requested changes

--- src/app/page/UserProfile/UserProfile.js
+++ src/app/page/UserProfile/UserProfile.js
@@ -76,38 +76,19 @@ const UserProfile = ({ history }) => {
     setProfile(_.cloneDeep(firebaseProfile));
     setView("view");
   }
-  async function saveProfileAction(e) {
+  function saveProfileAction(e) {
     e.preventDefault();
     try {
       !profile.address
-        ? console.log(null)
-        : await addressLookUp(profile.address).then((data) =>
+        ? firebase.updateProfile(profile)
+        : addressLookUp(profile.address).then((data) =>
             firebase.updateProfile({
               ...profile,
               addressMapPoint: { latitude: data.lat, longitude: data.long },
             })
           );
     } catch (error) {
-      if (error.response) {
-        /*
-         * The request was made and the server responded with a
-         * status code that falls out of the range of 2xx
-         */
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        /*
-         * The request was made but no response was received, `error.request`
-         * is an instance of XMLHttpRequest in the browser and an instance
-         * of http.ClientRequest in Node.js
-         */
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request and triggered an Error
-        console.log("Error", error.message);
-      }
-      console.log(error);
+      console.log("ERROR WHEN GETTiNG LOCATION", error);
     }
     setView("view");
   }
