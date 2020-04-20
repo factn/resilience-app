0ba3c1613424c2a459400448bc64a7d531d821fd
Paul Mendoza <doza@indiginis.com>
2020-04-20 10:16:11 -0400

    use tabs instead of stepper

--- src/app/page/Request/foodbox/FoodboxFlow.jsx
+++ src/app/page/Request/foodbox/FoodboxFlow.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
-import { Stepper, Step, StepLabel, StepConnector } from "@material-ui/core";
+import { Tabs, Tab, Paper } from "@material-ui/core";
 
 import { H1 } from "../styles";
 import { H4 } from "../../../component";
@@ -13,31 +12,18 @@ import ConfirmStep from "./ConfirmStep";
 const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: "100%",
-    marginBottom: "-3rem",
-  },
-  backButton: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-  buttonsContainer: {
-    display: "flex",
-    padding: "1rem",
-    bottom: "1rem",
   },
   content: {
     margin: "0rem 1rem",
-    minHeight: "calc(100vh -70px)",
+  },
+  tabMargin: {
+    margin: "1rem 0",
   },
 }));
 
-const steps = ["FOODBOX", "DELIVERY", "CONFIRM"];
 const stepComponents = [FoodboxStep, DeliveryStep, ConfirmStep];
 
 export default function FoodboxFlow() {
-  const history = useHistory();
   const classes = useStyles();
   const [activeStep, setActiveStep] = useState(0);
 
@@ -46,11 +32,7 @@ export default function FoodboxFlow() {
   };
 
   const handleBack = () => {
-    if (activeStep === 0) {
-      history.push("/request");
-    } else {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    }
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
   const ActiveComponent = stepComponents[activeStep];
@@ -58,15 +40,13 @@ export default function FoodboxFlow() {
   return (
     <div className={classes.root}>
       <H1>Food Box Delivery</H1>
-      <CustomStepper activeStep={activeStep} alternativeLabel connector={<CustomConnector />}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <CustomStepLabel StepIconComponent={CustomStepIcon}>{label}</CustomStepLabel>
-          </Step>
-        ))}
-      </CustomStepper>
-      {/* <div className={classes.content}>{getStepContent(activeStep, handleNext, handleBack)} {
-        }</div> */}
+      <Paper className={classes.tabMargin} elevation={3} square>
+        <Tabs value={activeStep} indicatorColor="primary" textColor="primary" centered>
+          <CustomTab icon={<H4>1</H4>} label="FOODBOX" disableRipple />
+          <CustomTab icon={<H4>2</H4>} label="DELIVERY" disableRipple />
+          <CustomTab icon={<H4>3</H4>} label="CONFIRM" disableRipple />
+        </Tabs>
+      </Paper>
       <div className={classes.content}>
         <ActiveComponent onBack={handleBack} onNext={handleNext}></ActiveComponent>
       </div>
@@ -74,37 +54,8 @@ export default function FoodboxFlow() {
   );
 }
 
-const CustomStepper = withStyles((theme) => ({
+const CustomTab = withStyles((theme) => ({
   root: {
-    padding: "1rem 1.5rem 0",
-    boxShadow: "0px 2px 10px rgba(0,0,0,.3)",
-    marginTop: "1rem",
-    marginBottom: "1rem",
+    color: theme.color.vibrantPurple,
   },
-}))(Stepper);
-
-const CustomConnector = withStyles({
-  line: {
-    opacity: 0,
-  },
-})(StepConnector);
-
-const CustomStepLabel = withStyles((theme) => ({
-  active: {
-    borderBottom: `solid ${theme.color.vibrantPurple}`,
-  },
-  labelContainer: {
-    marginTop: "-1rem",
-  },
-  label: {
-    // only way to override these... =(
-    color: `${theme.color.vibrantPurple} !important`,
-    fontWeight: "600 !important",
-    fontSize: ".9rem",
-    paddingBottom: ".5rem",
-  },
-}))(StepLabel);
-
-const CustomStepIcon = (props) => {
-  return <H4>{props.icon}</H4>;
-};
+}))(Tab);
