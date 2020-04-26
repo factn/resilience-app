a3f4e15f80fdf782a54d3661c168d88449586d08
Taylor <wwwaaavvveeerrr@gmail.com>
2020-04-26 00:58:08 -0400

    version 1/2, style,s waiting for more desginer input

--- src/app/dashboard/Missions/CreateMission.jsx
+++ src/app/dashboard/Missions/CreateMission.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// components
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
@@ -10,6 +11,14 @@ import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+//icons
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import PeopleIcon from "@material-ui/icons/People";
+import PanToolIcon from "@material-ui/icons/PanTool";
+import PhoneIcon from "@material-ui/icons/Phone";
+import AddLocationIcon from "@material-ui/icons/AddLocation";
+import QueryBuilderIcon from "@material-ui/icons/QueryBuilder";
 
 const useStyles = makeStyles((theme) => ({
   addMissionContainer: {
@@ -18,8 +27,8 @@ const useStyles = makeStyles((theme) => ({
     padding: "15px 25px",
     position: "absolute",
     width: "721px",
-    left: "143px",
-    top: "132px",
+    left: "83px",
+    top: "83px",
     boxShadow: "2px 10px 6px rgba(0, 0, 0, 0.19)",
   },
   title: {
@@ -35,19 +44,77 @@ const useStyles = makeStyles((theme) => ({
     color: "#3739B5",
     fontSize: "42px",
     margin: "18px 9px",
+    fontWeight: "bold",
   },
-  inputHeader: {},
-  input: {},
+  inputHeader: {
+    color: "#3739B5",
+    fontWeight: "600",
+    fontSize: "26px",
+    margin: "15px 0",
+    textAlign: "left",
+  },
+  select: {
+    maxWidth: "50%",
+  },
   form: {
     padding: "10px",
   },
-  optionalTag: {},
+  optionalTagBox: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  optionalTag: {
+    color: "grey",
+    occupancy: "0.5",
+    fontSize: "10px",
+    padding: "0 5px",
+    margin: "15px 0",
+  },
   dualForm: {
     display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  inputHeader2: {},
-  bigTextField: {},
-  button: {},
+  form2: {
+    margin: "10px",
+    width: "100%",
+  },
+  inputHeader2: {
+    textAlign: "left",
+    fontWeight: "600",
+    fontSize: "16px",
+    lineHeight: "24px",
+    padding: "5px",
+    marginBottom: "5px",
+  },
+  optionalTag2: {
+    color: "grey",
+    occupancy: "0.5",
+    lineHeight: "24px",
+    fontSize: "10px",
+    padding: "0 5px",
+    margin: "4px 0",
+  },
+  header2Icons: {
+    color: "#3739B5",
+    marginTop: "3px",
+  },
+  dualSelect: {
+    maxWidth: "100%",
+  },
+  dualText: {
+    margin: "10px 0",
+  },
+  bigTextField: {
+    heigh: "150px",
+  },
+  button: {
+    background: "#3739B5",
+    margin: "15px 0",
+    color: "white",
+    padding: "10px",
+    width: "100%",
+  },
 }));
 
 const CreateMission = () => {
@@ -62,63 +129,179 @@ const CreateMission = () => {
         <Divider variant="middle" />
         <Typography className={classes.inputHeader}>Mission Type</Typography>
         <FormControl className={classes.form}>
-          <InputLabel id="smt">Select Mission Type</InputLabel>
-          <Select className={classes.input} labelId="smt">
+          <Select variant="outlined" className={classes.select} value="default" labelId="smt">
+            <MenuItem value="default">Select Mission Type</MenuItem>
             <MenuItem value="foodBox">Food Box</MenuItem>
+            {/* THIS NEEDS TO SPAWN NEW MENU SPECIFIC TO MISSION */}
           </Select>
         </FormControl>
         <Divider variant="middle" />
-        <Typography className={classes.inputHeader}>Funding Details </Typography>
-        <Typography className={classes.optionalTag}>- optional</Typography>
+        <div className={classes.optionalTagBox}>
+          <Typography className={classes.inputHeader}>Funding Details </Typography>
+          <Typography className={classes.optionalTag}> - optional</Typography>
+        </div>
         <FormControl className={classes.form}>
-          <InputLabel id="fd">Not Funded Yet</InputLabel>
-          <Select className={classes.input} labelId="fd">
+          <Select
+            variant="outlined"
+            className={classes.select}
+            value="default"
+            startAdornment={
+              <InputAdornment position="start">
+                <AttachMoneyIcon style={{ color: "#3739B5" }} />
+              </InputAdornment>
+            }
+          >
+            {" "}
+            {/* THIS WILL HAVE TO HAVE A DYNAMIC INITIAL VALUE */}
+            <MenuItem value="default">Not Funded Yet</MenuItem>
             <MenuItem value={0}>Needs Funding</MenuItem>
           </Select>
         </FormControl>
         <Divider variant="middle" />
+
         <div className={classes.dualForm}>
-          <FormControl className={classes.form}>
-            <Typography className={classes.inputHeader2}>Recipient Details </Typography>
-            <InputLabel id="name">Name</InputLabel>
-            <TextField labelId="name" placeholder="Name" />
+          <FormControl className={classes.form2}>
+            <div className={classes.optionalTagBox}>
+              <Typography className={classes.inputHeader2}>Recipient Details </Typography>
+              <PeopleIcon className={classes.header2Icons} />
+            </div>
+            <Select
+              className={classes.dualSelect}
+              labelId="name"
+              placeholder="Name"
+              value="default"
+              variant="outlined"
+            >
+              <MenuItem value="default"> Select Name</MenuItem>{" "}
+              {/* NEED TO DYNAMICALLY SET NAMES */}
+            </Select>
             <br />
-            <InputLabel id="phone">Phone</InputLabel>
-            <TextField labelId="phone" placeholder="Enter Phone Number" />
+            <TextField
+              variant="outlined"
+              className={classes.dualText}
+              labelId="phone"
+              placeholder="Enter Phone Number"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PhoneIcon style={{ color: "#3739B5" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />
           </FormControl>
-          <FormControl className={classes.form}>
-            <Typography className={classes.inputHeader2}>Volunteer Details </Typography>
-            <Typography className={classes.optionalTag}>- optional</Typography>
-            <InputLabel id="name0">Name</InputLabel>
-            <TextField labelId="name0" placeholder="Name" />
+          <FormControl className={classes.form2}>
+            <div className={classes.optionalTagBox}>
+              <Typography className={classes.inputHeader2}>Volunteer Details </Typography>
+              <PanToolIcon className={classes.header2Icons} style={{ fontSize: "large" }} />
+              <Typography className={classes.optionalTag2}> - optional</Typography>
+            </div>
+            <Select
+              className={classes.dualSelect}
+              labelId="name0"
+              placeholder="Name"
+              value="default"
+              variant="outlined"
+            >
+              <MenuItem value="default"> Select Name</MenuItem>{" "}
+              {/* NEED TO DYNAMICALLY SET NAMES */}
+            </Select>
             <br />
-            <InputLabel id="phone0">Phone</InputLabel>
-            <TextField labelId="phone0" placeholder="Enter Phone Number" />
+            <TextField
+              variant="outlined"
+              className={classes.dualText}
+              labelId="phone0"
+              placeholder="Enter Phone Number"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PhoneIcon style={{ color: "#3739B5" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />
           </FormControl>
         </div>
+
         <Divider variant="middle" />
         <div className={classes.dualForm}>
-          <FormControl className={classes.form}>
+          <FormControl className={classes.form2}>
             <Typography className={classes.inputHeader2}>Drop Off Details </Typography>
-            <InputLabel id="location">Address</InputLabel>
-            <TextField labelId="location" placeholder="Enter Address" />
+            <TextField
+              className={classes.dualText}
+              variant="outlined"
+              labelId="location"
+              placeholder="Enter Address"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AddLocationIcon style={{ color: "#3739B5" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />{" "}
+            {/** NEED TO RUN ADDRESS SEARCH ON THESE TO STORE THE LOCATION IN THE BACKEND */}
             <br />
-            <InputLabel id="dod">Drop Off Date</InputLabel>
-            <TextField labelId="dod" placeholder="Enter Date" />
+            <TextField
+              type="date"
+              className={classes.dualText}
+              variant="outlined"
+              labelId="dod"
+              placeholder="Enter Drop Off Date Date "
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <QueryBuilderIcon style={{ color: "#3739B5" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />
           </FormControl>
-          <FormControl className={classes.form}>
+          <FormControl className={classes.form2}>
             <Typography className={classes.inputHeader2}>Pick Up Details </Typography>
-            <InputLabel id="address">Address</InputLabel>
-            <TextField labelId="address" placeholder="Enter Location" />
+            <TextField
+              className={classes.dualText}
+              variant="outlined"
+              labelId="address"
+              placeholder="Enter Address"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AddLocationIcon style={{ color: "#3739B5" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />{" "}
+            {/** NEED TO RUN ADDRESS SEARCH ON THESE TO STORE THE LOCATION IN THE BACKEND */}
             <br />
-            <InputLabel id="pud">Pick Up Date</InputLabel>
-            <TextField labelId="pud" placeholder="Enter Date" />
+            <TextField
+              type="date"
+              className={classes.dualText}
+              variant="outlined"
+              labelId="pud"
+              placeholder="Enter Pick Up Date Date"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <QueryBuilderIcon style={{ color: "#3739B5" }} />
+                  </InputAdornment>
+                ),
+              }}
+            />
           </FormControl>
         </div>
         <Divider variant="middle" />
-        <Typography className={classes.inputHeader2}>Drop Off Instructions / Comments</Typography>
-        <Typography className={classes.optionalTag}>- optional</Typography>
-        <TextField className={classes.bigTextField} multiline></TextField>
+
+        <div className={classes.optionalTagBox}>
+          <Typography className={classes.inputHeader2}>Drop Off Instructions / Comments</Typography>
+          <Typography className={classes.optionalTag2}> - optional</Typography>
+        </div>
+        <TextField
+          className={classes.bigTextField}
+          variant="outlined"
+          placeholder="Enter any notes or comments about this mission here."
+          multiline
+        ></TextField>
         <Button className={classes.button}>Create Mission</Button>
       </Typography>
     </Container>
