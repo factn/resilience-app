eeef272d190d4bd260067439c5c57150a87cc414
D. Alvarado <laredotornado@gmail.com>
2020-04-26 13:19:17 -0500

    Merging.

--- src/app/page/Home/VolunteerHome.jsx
+++ src/app/page/Home/VolunteerHome.jsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-import { AppBar, Box, Paper, Tab, Tabs, Typography } from "@material-ui/core";
-=======
 import { Box, Paper, Tab, Tabs, Typography } from "@material-ui/core";
->>>>>>> ba3f049aefe3f3a7f7fac802b9aa46e0b3a3184b
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 
 import User from "../../model/User";
-<<<<<<< HEAD
-import {
-  getAllAssignedMissions,
-  getAllStartedMissions,
-  getAllAvailableMissions,
-} from "./missionHelpers";
-=======
 import { getAllAssignedMissions, getAllStartedMissions } from "./missionHelpers";
->>>>>>> ba3f049aefe3f3a7f7fac802b9aa46e0b3a3184b
 import VolunteerHomeMissionList from "./VolunteerHomeMissionList";
 
 const useStyles = makeStyles((theme) => ({
@@ -45,10 +33,6 @@ export default function VolunteerHome({ currentUser }) {
       updateAvailableMissions(availableMissions);
       updateAssignedMissions(getAllAssignedMissions(missions, currentUser));
       updateStartedMissions(getAllStartedMissions(missions, currentUser));
-<<<<<<< HEAD
-      updateAvailableMissions(getAllAvailableMissions(missions, currentUser));
-=======
->>>>>>> ba3f049aefe3f3a7f7fac802b9aa46e0b3a3184b
     };
 
     fetchAllAssociatedMissions();
@@ -74,9 +58,6 @@ export default function VolunteerHome({ currentUser }) {
 
   const handleDeliveringMissionsFromStarted = (missionId) => {
     User.deliverMission(currentUser.uid, missionId);
-<<<<<<< HEAD
-    const mission = startedMissions.filter((m) => m.id === missionId);
-
     updateStartedMissions(startedMissions.filter((m) => m.id !== missionId));
   };
 
@@ -84,15 +65,6 @@ export default function VolunteerHome({ currentUser }) {
   const acceptedLabel = "Accepted (" + acceptedMissions.length + ")";
   const startedLabel = "Started (" + startedMissions.length + ")";
 
-=======
-    updateStartedMissions(startedMissions.filter((m) => m.id !== missionId));
-  };
-
-  const availableLabel = "Available (" + availableMissions.length + ")";
-  const acceptedLabel = "Accepted (" + acceptedMissions.length + ")";
-  const startedLabel = "Started (" + startedMissions.length + ")";
-
->>>>>>> ba3f049aefe3f3a7f7fac802b9aa46e0b3a3184b
   return (
     <div className={classes.root}>
       <Paper className={classes.tabMargin} elevation={3} square>
