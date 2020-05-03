9fd99e0f2227a2f33ed2b667e99b9b9ebd4af32c
D. Alvarado <laredotornado@gmail.com>
2020-05-02 20:15:56 -0500

    Merging with upstream/master.

--- src/app/page/MissionCreate/MissionCreate.jsx
+++ src/app/page/MissionCreate/MissionCreate.jsx
@@ -48,11 +48,7 @@ function MakeMission({ history }) {
 
       //create mission object
       const mission = {
-<<<<<<< HEAD
-        organisationId: "1", // PLACEHOLDER -> user doesn'T have an organisation id yet
-=======
         organizationId: "1", // PLACEHOLDER -> user doesn'T have an organization id yet
->>>>>>> ca14cd0e4fe0ed4b4f46bb381f0007005eda153b
         volunteerId: volunteerId,
         title: payload.title,
         description: payload.description,
