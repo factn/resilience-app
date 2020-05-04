320ade8dcce72f9683396c8c63e23e77d6b01e15
Paul Mendoza <doza@indiginis.com>
2020-05-03 23:14:23 -0400

    worthless test

--- src/app/model/User.old-test.ts
+++ src/app/model/User.old-test.ts
@@ -1,242 +1,242 @@
-import ReduxFirebase from "react-redux-firebase";
+// import ReduxFirebase from "react-redux-firebase";
 
-import Mission from "./Mission";
-import { MissionStatus } from "./schema";
-import Users from "./User";
+// import Mission from "./Mission";
+// import { MissionStatus } from "./schema";
+// import Users from "./User";
 
-function mockAcceptMissionRepo({ getByIdReturn, throwCollectionDocError, throwUpdateError }: any) {
-  const mockUpdate = throwUpdateError
-    ? jest.fn().mockImplementation(() => {
-        throw Error("Error");
-      })
-    : jest.fn();
+// function mockAcceptMissionRepo({ getByIdReturn, throwCollectionDocError, throwUpdateError }: any) {
+//   const mockUpdate = throwUpdateError
+//     ? jest.fn().mockImplementation(() => {
+//         throw Error("Error");
+//       })
+//     : jest.fn();
 
-  const mockGetById = jest.spyOn(Mission, "getByUid").mockResolvedValue(getByIdReturn);
+//   const mockGetById = jest.spyOn(Mission, "getByUid").mockResolvedValue(getByIdReturn);
 
-  const collection = {
-    doc: jest.fn().mockReturnValue({
-      collection: jest.fn().mockReturnValue({
-        doc: jest.fn().mockReturnValue({
-          update: mockUpdate,
-        }),
-      }),
-    }),
-  };
+//   const collection = {
+//     doc: jest.fn().mockReturnValue({
+//       collection: jest.fn().mockReturnValue({
+//         doc: jest.fn().mockReturnValue({
+//           update: mockUpdate,
+//         }),
+//       }),
+//     }),
+//   };
 
-  jest.spyOn(ReduxFirebase, "getFirebase").mockReturnValue({
-    firestore: jest.fn(() => ({ collection: jest.fn(() => collection) })),
-  } as any);
+//   jest.spyOn(ReduxFirebase, "getFirebase").mockReturnValue({
+//     firestore: jest.fn(() => ({ collection: jest.fn(() => collection) })),
+//   } as any);
 
-  return {
-    mockGetById,
-    mockUpdate,
-  };
-}
+//   return {
+//     mockGetById,
+//     mockUpdate,
+//   };
+// }
 
-describe("User", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+// describe("User", () => {
+//   afterEach(() => {
+//     jest.clearAllMocks();
+//   });
 
-  /*  
-  function mockBaseRepo({
-  existsReturn,
-  mockDataReturn,
-  throwCollectionDocError,
-  throwUpdateError,
-  throwWhereError,
-}) {
-  const mockData = jest.fn().mockImplementation(() => mockDataReturn);
-  const mockUpdate = throwUpdateError
-    ? jest.fn().mockImplementation(() => {
-        throw Error("Error");
-      })
-    : jest.fn();
+//   /*
+//   function mockBaseRepo({
+//   existsReturn,
+//   mockDataReturn,
+//   throwCollectionDocError,
+//   throwUpdateError,
+//   throwWhereError,
+// }) {
+//   const mockData = jest.fn().mockImplementation(() => mockDataReturn);
+//   const mockUpdate = throwUpdateError
+//     ? jest.fn().mockImplementation(() => {
+//         throw Error("Error");
+//       })
+//     : jest.fn();
 
-  const mockGet = {
-    exists: existsReturn,
-    data: mockData,
-  };
+//   const mockGet = {
+//     exists: existsReturn,
+//     data: mockData,
+//   };
 
-  const mockDocFn = throwCollectionDocError
-    ? jest.fn().mockImplementation(() => {
-        throw Error("Error");
-      })
-    : jest.fn().mockReturnValue({
-        get: jest.fn().mockResolvedValue(mockGet),
-        update: mockUpdate,
-        exists: existsReturn,
-      });
-  const doc = {
-    get: jest.fn().mockResolvedValue({
-      empty: !mockDataReturn.length,
-      docs: mockDataReturn,
-    }),
-  };
-  const mockWhereFn = jest.fn().mockImplementation(() =>
-    throwWhereError
-      ? jest.fn().mockImplementation(() => {
-          throw Error("Error");
-        })
-      : doc
-  );
-  const collection = {
-    doc: mockDocFn,
-    where: mockWhereFn,
-  };
+//   const mockDocFn = throwCollectionDocError
+//     ? jest.fn().mockImplementation(() => {
+//         throw Error("Error");
+//       })
+//     : jest.fn().mockReturnValue({
+//         get: jest.fn().mockResolvedValue(mockGet),
+//         update: mockUpdate,
+//         exists: existsReturn,
+//       });
+//   const doc = {
+//     get: jest.fn().mockResolvedValue({
+//       empty: !mockDataReturn.length,
+//       docs: mockDataReturn,
+//     }),
+//   };
+//   const mockWhereFn = jest.fn().mockImplementation(() =>
+//     throwWhereError
+//       ? jest.fn().mockImplementation(() => {
+//           throw Error("Error");
+//         })
+//       : doc
+//   );
+//   const collection = {
+//     doc: mockDocFn,
+//     where: mockWhereFn,
+//   };
 
-  jest.spyOn(ReduxFirebase, "getFirebase").mockReturnValue({
-    firestore: jest.fn(() => ({ collection: jest.fn(() => collection) })),
-  });
-  jest.spyOn(users, "getCollection").mockImplementation(() => collection);
+//   jest.spyOn(ReduxFirebase, "getFirebase").mockReturnValue({
+//     firestore: jest.fn(() => ({ collection: jest.fn(() => collection) })),
+//   });
+//   jest.spyOn(users, "getCollection").mockImplementation(() => collection);
 
-  return {
-    mockDocFn,
-    mockData,
-    mockUpdate,
-    mockWhereFn,
-  };
-}
-  
-  describe("#getIdByDisplayName", () => {
-    const displayName = "username";
-    const volunteerUid = "aabbbccc";
-    let user = {
-      id: volunteerUid,
-      displayName: displayName,
-    };
+//   return {
+//     mockDocFn,
+//     mockData,
+//     mockUpdate,
+//     mockWhereFn,
+//   };
+// }
 
-    it("looks up user id by display name", async () => {
-      const { mockWhereFn } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: [user],
-        throwCollectionDocError: false,
-        throwWhereError: false,
-        throwUpdateError: false,
-      });
+//   describe("#getIdByDisplayName", () => {
+//     const displayName = "username";
+//     const volunteerUid = "aabbbccc";
+//     let user = {
+//       id: volunteerUid,
+//       displayName: displayName,
+//     };
 
-      let result = await users.getIdByDisplayName(displayName);
+//     it("looks up user id by display name", async () => {
+//       const { mockWhereFn } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: [user],
+//         throwCollectionDocError: false,
+//         throwWhereError: false,
+//         throwUpdateError: false,
+//       });
 
-      expect(result).toBe(volunteerUid);
-      expect(mockWhereFn).toBeCalledWith("displayName", "==", displayName);
-    });
+//       let result = await users.getIdByDisplayName(displayName);
 
-    it("returns error if firebase where bombs", async () => {
-      const { mockWhereFn } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: [user],
-        throwCollectionDocError: false,
-        throwWhereError: true,
-        throwUpdateError: false,
-      });
+//       expect(result).toBe(volunteerUid);
+//       expect(mockWhereFn).toBeCalledWith("displayName", "==", displayName);
+//     });
 
-      await expect(users.getIdByDisplayName(displayName)).rejects.toThrow(Error);
-      expect(mockWhereFn).toBeCalledWith("displayName", "==", displayName);
-    });
+//     it("returns error if firebase where bombs", async () => {
+//       const { mockWhereFn } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: [user],
+//         throwCollectionDocError: false,
+//         throwWhereError: true,
+//         throwUpdateError: false,
+//       });
 
-    it("no user found by display name", async () => {
-      const { mockWhereFn } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: [],
-        throwCollectionDocError: false,
-        throwWhereError: false,
-        throwUpdateError: false,
-      });
+//       await expect(users.getIdByDisplayName(displayName)).rejects.toThrow(Error);
+//       expect(mockWhereFn).toBeCalledWith("displayName", "==", displayName);
+//     });
 
-      await expect(users.getIdByDisplayName(displayName)).rejects.toThrow(Error);
-      expect(mockWhereFn).toBeCalledWith("displayName", "==", displayName);
-    });
-  });
-*/
+//     it("no user found by display name", async () => {
+//       const { mockWhereFn } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: [],
+//         throwCollectionDocError: false,
+//         throwWhereError: false,
+//         throwUpdateError: false,
+//       });
 
-  describe("#unvolunteerMission", () => {
-    const volunteerUid = "aabbbccc";
-    let mission = {
-      volunteerUid: "",
-      status: "",
-    };
+//       await expect(users.getIdByDisplayName(displayName)).rejects.toThrow(Error);
+//       expect(mockWhereFn).toBeCalledWith("displayName", "==", displayName);
+//     });
+//   });
+// */
 
-    beforeEach(() => {
-      mission.volunteerUid = volunteerUid;
-      mission.status = MissionStatus.assigned;
-    });
+//   describe("#unvolunteerMission", () => {
+//     const volunteerUid = "aabbbccc";
+//     let mission = {
+//       volunteerUid: "",
+//       status: "",
+//     };
 
-    /*  it("unassigns volunteer if missionUid exists", async () => {
-      const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: mission,
-        throwCollectionDocError: false,
-        throwUpdateError: false,
-      });
+//     beforeEach(() => {
+//       mission.volunteerUid = volunteerUid;
+//       mission.status = MissionStatus.assigned;
+//     });
 
-      await users.unvolunteerMission(missionUid);
+//     /*  it("unassigns volunteer if missionUid exists", async () => {
+//       const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: mission,
+//         throwCollectionDocError: false,
+//         throwUpdateError: false,
+//       });
 
-      expect(mockDocFn).toBeCalledWith(missionUid);
-      expect(mockData).toBeCalledTimes(1);
-      const expected = {
-        volunteerUid: "",
-        status: MissionStatus.unassigned,
-      };
-      expect(mockUpdate).toBeCalledWith(expected);
-    });
+//       await users.unvolunteerMission(missionUid);
 
-    it("throws an error if doc.exists is false", async () => {
-      const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
-        existsReturn: false,
-        mockDataReturn: mission,
-        throwCollectionDocError: false,
-        throwUpdateError: false,
-      });
+//       expect(mockDocFn).toBeCalledWith(missionUid);
+//       expect(mockData).toBeCalledTimes(1);
+//       const expected = {
+//         volunteerUid: "",
+//         status: MissionStatus.unassigned,
+//       };
+//       expect(mockUpdate).toBeCalledWith(expected);
+//     });
 
-      await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
+//     it("throws an error if doc.exists is false", async () => {
+//       const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
+//         existsReturn: false,
+//         mockDataReturn: mission,
+//         throwCollectionDocError: false,
+//         throwUpdateError: false,
+//       });
 
-      expect(mockDocFn).toBeCalledWith(missionUid);
-      expect(mockData).not.toBeCalled();
-      expect(mockUpdate).not.toBeCalled();
-    });
+//       await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
 
-    it("throws an error if missionUid doesn't exist", async () => {
-      const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: null,
-        throwCollectionDocError: false,
-        throwUpdateError: false,
-      });
+//       expect(mockDocFn).toBeCalledWith(missionUid);
+//       expect(mockData).not.toBeCalled();
+//       expect(mockUpdate).not.toBeCalled();
+//     });
 
-      await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
+//     it("throws an error if missionUid doesn't exist", async () => {
+//       const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: null,
+//         throwCollectionDocError: false,
+//         throwUpdateError: false,
+//       });
 
-      expect(mockDocFn).toBeCalledWith(missionUid);
-      expect(mockData).toBeCalledTimes(1);
-      expect(mockUpdate).not.toBeCalled();
-    }); 
+//       await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
 
-    it("throws an error if collection.doc throws an error", async () => {
-      const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: mission,
-        throwCollectionDocError: true,
-        throwUpdateError: false,
-      });
+//       expect(mockDocFn).toBeCalledWith(missionUid);
+//       expect(mockData).toBeCalledTimes(1);
+//       expect(mockUpdate).not.toBeCalled();
+//     });
 
-      await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
+//     it("throws an error if collection.doc throws an error", async () => {
+//       const { mockData, mockWhereFn,  mockDocFn, mockUpdate } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: mission,
+//         throwCollectionDocError: true,
+//         throwUpdateError: false,
+//       });
 
-      expect(mockDocFn).toBeCalledWith(missionUid);
-      expect(mockData).not.toBeCalled();
-      expect(mockUpdate).not.toBeCalled();
-    });
+//       await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
 
-    it("throws an error if doc.update throws an error", async () => {
-      const { mockData, mockWhereFn,  mockDocFn } = mockBaseRepo({
-        existsReturn: true,
-        mockDataReturn: mission,
-        throwCollectionDocError: false,
-        throwUpdateError: true,
-      });
+//       expect(mockDocFn).toBeCalledWith(missionUid);
+//       expect(mockData).not.toBeCalled();
+//       expect(mockUpdate).not.toBeCalled();
+//     });
 
-      await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
+//     it("throws an error if doc.update throws an error", async () => {
+//       const { mockData, mockWhereFn,  mockDocFn } = mockBaseRepo({
+//         existsReturn: true,
+//         mockDataReturn: mission,
+//         throwCollectionDocError: false,
+//         throwUpdateError: true,
+//       });
 
-      expect(mockDocFn).toBeCalledWith(missionUid);
-      expect(mockData).toBeCalledTimes(1);
-    });*/
-  });
-});
+//       await expect(users.unvolunteerMission(missionUid)).rejects.toThrow(Error);
+
+//       expect(mockDocFn).toBeCalledWith(missionUid);
+//       expect(mockData).toBeCalledTimes(1);
+//     });*/
+//   });
+// });
