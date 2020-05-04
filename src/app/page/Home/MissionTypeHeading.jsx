a00949f32e25ab5ce5112821ac1991cd62b38dda
Amy Lo <amyamy86@gmail.com>
2020-05-03 23:25:15 -0700

    feat: [122] Line up icon with text in MissionTypeHeading

--- src/app/page/Home/MissionTypeHeading.jsx
+++ src/app/page/Home/MissionTypeHeading.jsx
@@ -6,6 +6,8 @@ import { makeStyles } from "@material-ui/core/styles";
 const missionTypeStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.color.lightgrey,
+    display: "flex",
+    alignItems: "center",
     marginBottom: "1rem",
     padding: "0.5rem 1rem",
     textAlign: "left",
