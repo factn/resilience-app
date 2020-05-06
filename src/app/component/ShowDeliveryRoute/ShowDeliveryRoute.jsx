5d32c5ec7bb040291b74eb741fdfa5a610c73e77
Miles Thompson <utunga@gmail.com>
2020-05-07 01:48:39 +1200

    Run npm format

--- src/app/component/ShowDeliveryRoute/ShowDeliveryRoute.jsx
+++ src/app/component/ShowDeliveryRoute/ShowDeliveryRoute.jsx
@@ -16,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const ShowDeliveryRoute = ({ missions, isLoaded, isEmpty }) => {
+const ShowDeliveryRoute = ({ isEmpty, isLoaded, missions }) => {
   const classes = useStyles();
   const [googleMapsUrl, setGoogleMapsUrl] = useState("");
 
