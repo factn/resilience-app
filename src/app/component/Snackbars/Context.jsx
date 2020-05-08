4cd078bbb597709ee35aa216f0b948ca1b13b820
Joshua Wu <joshua.wu.0910@gmail.com>
2020-05-08 10:55:03 -0700

    Add Snackbar context

---
+++ src/app/component/Snackbars/Context.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const SnackbarContext = React.createContext();
+
+const defaultSnackbar = {
+  autoHideDuration: 4000,
+  message: "",
+  open: false,
+  type: "success",
+};
+
+class SnackbarProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...defaultSnackbar,
+    };
+
+    this.handleCloseSnackbar = this.handleCloseSnackbar.bind(this);
+    this.handleUpdateSnackbar = this.handleUpdateSnackbar.bind(this);
+  }
+
+  handleCloseSnackbar() {
+    return this.setState({ ...defaultSnackbar });
+  }
+
+  /**
+   * Callback to update snackbar context provider.
+   * snackbarProps should be valid props of Snackbar
+   *
+   * @param {autohideDuration, message, open, type} snackbarProps
+   * @returns void
+   * @memberof SnackbarProvider
+   */
+  handleUpdateSnackbar(snackbarProps) {
+    return this.setState({ ...snackbarProps });
+  }
+
+  render() {
+    return (
+      <SnackbarContext.Provider
+        value={{
+          snackbar: this.state,
+          closeSnackbar: this.handleCloseSnackbar,
+          updateSnackbar: this.handleUpdateSnackbar,
+        }}
+      >
+        {this.props.children}
+      </SnackbarContext.Provider>
+    );
+  }
+}
+
+const SnackbarConsumer = SnackbarContext.Consumer;
+
+export { SnackbarContext, SnackbarConsumer, SnackbarProvider };
