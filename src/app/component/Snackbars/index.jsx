ef1a0bbc7925673fbd24fc9837d5153fa93cba03
Joshua Wu <joshua.wu.0910@gmail.com>
2020-05-08 11:03:26 -0700

    Add snackbar provider to app and first consumer in userProfile

--- src/app/component/Snackbars/index.jsx
+++ src/app/component/Snackbars/index.jsx
@@ -1,4 +1,12 @@
+/* Deprecated */
 import ErrorSnackbar from "./ErrorSnackbar";
 import SuccessSnackbar from "./SuccessSnackbar";
+/* end Deprecated */
+
+import Snackbar from "./Snackbar";
+import * as Context from "./Context";
+
+Snackbar.Context = Context;
 
 export { ErrorSnackbar, SuccessSnackbar };
+export default Snackbar;
