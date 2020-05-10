e54ffc344d420907704db333ec7e60503b2108d2
D. Alvarado <laredotornado@gmail.com>
2020-05-09 21:07:29 -0500

    Added button components for reuse in mission details card.

---
+++ src/app/component/DeliverMissionButton.jsx
@@ -0,0 +1,20 @@
+import { Grid } from "@material-ui/core";
+import { Button } from "./";
+import React from "react";
+
+/**
+ * Button that facliitates confirmation of mission delivery. 
+ *
+ * @component
+ */
+const DeliverMissionButton = ({ buttonClass, deliverMission }) => {
+  return ( 
+    <Button size="medium" onClick={deliverMission} className={buttonClass}>
+      <Grid container direction="row" spacing={1} alignItems={'center'} justify={'center' } style={{ width: "100%" }} >
+        <Grid item>CONFIRM DELIVERY</Grid>
+      </Grid>
+    </Button>
+  ); 
+};
+
+export default DeliverMissionButton; 
---
+++ src/app/component/StartMissionButton.jsx
@@ -0,0 +1,24 @@
+import { Grid } from "@material-ui/core";
+import { Button } from "./";
+import React from "react";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+
+/**
+ * Button that facliitates starting a mission. 
+ *
+ * @component
+ */
+const StartMissionButton = ({ buttonClass, startMission }) => {
+  return ( 
+    <Button size="medium" onClick={startMission} className={buttonClass}>
+      <Grid container direction="row" spacing={1} alignItems={'center'} justify={'center' } style={{ width: "100%" }} >
+        <Grid item>
+          <PlayCircleFilledIcon />
+        </Grid>
+        <Grid item>START</Grid>
+      </Grid>
+    </Button>
+  ); 
+};
+
+export default StartMissionButton; 
