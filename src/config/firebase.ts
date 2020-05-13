ccd1347097c96fba117e64772051c499cda60c05
Paul Mendoza <doza@indiginis.com>
2020-05-12 21:11:23 -0400

    trying to reconcile two firebase projects

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+export const firebaseConfigDev = {
   apiKey: "AIzaSyBnuxL3OFy6EbR5T7XlkehkyXbOxnswQtQ",
   authDomain: "mutualaid-757f6.firebaseapp.com",
   databaseURL: "https://mutualaid-757f6.firebaseio.com",
@@ -7,6 +7,17 @@ export const firebaseConfig = {
   messagingSenderId: "57281805127",
   appId: "1:57281805127:web:7c025105ef5fb5e5232a62",
   measurementId: "G-7L9JFWQHS2",
+};
+
+export const firebaseConfigProd = {
+  apiKey: "AIzaSyCF1uIzrdqvIxTlGb0jOhAsMdZ7aPy2jdY",
+  authDomain: "resilienceapp-6a0dd.firebaseapp.com",
+  databaseURL: "https://resilienceapp-6a0dd.firebaseio.com",
+  projectId: "resilienceapp-6a0dd",
+  storageBucket: "resilienceapp-6a0dd.appspot.com",
+  messagingSenderId: "621108591526",
+  appId: "1:621108591526:web:c2a2acb855c8ec5acb2d67",
+  measurementId: "G-6EBQJ17N44",
 };
 
 export const rfConfig = {
