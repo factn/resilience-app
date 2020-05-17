54638521c8bb24df73abe1383f780ce99e2a23fd
Paul Mendoza <doza@indiginis.com>
2020-05-17 15:53:02 -0400

    starting details

--- src/app/page/Recipient/RequestsList.tsx
+++ src/app/page/Recipient/RequestsList.tsx
@@ -1,5 +1,16 @@
-import React from "react";
-import { Card, CardHeader, makeStyles, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  makeStyles,
+  Typography,
+  CardActionArea,
+  CardActions,
+  Collapse,
+  Button,
+  Box,
+  Divider,
+} from "@material-ui/core";
 import { FiberNew, PlayCircleFilled, NotificationImportant, CheckCircle } from "@material-ui/icons";
 import styled from "styled-components";
 
@@ -8,6 +19,8 @@ import {
   MissionStatus as Status,
   MissionFundedStatus as FundedStatus,
 } from "../../model/schema";
+import { useOrganization } from "../../model";
+import { ReactComponent as AppleIcon } from "../../../img/apple.svg";
 
 type Props = {
   missions: MissionInterface[];
@@ -22,10 +35,23 @@ const useStyles = makeStyles((theme) => ({
   card: {
     marginTop: "1rem",
   },
+  appleIcon: {
+    marginTop: ".rem",
+    height: "1.5rem",
+    width: "1.5rem",
+  },
+  cardHeader: {
+    alignItems: "end",
+  },
+  cardActions: {
+    justifyContent: "center",
+  },
 }));
 
 function RecipientMissionCard({ mission }: { mission: MissionInterface }) {
   const classes = useStyles();
+  const [open, setOpen] = useState(true);
+  const toggle = () => setOpen(!open);
 
   return (
     <>
@@ -35,10 +61,26 @@ function RecipientMissionCard({ mission }: { mission: MissionInterface }) {
           fundedStatus={mission.fundedStatus}
           createdDate={mission.createdDate}
         ></TopBar>
-        <CardHeader
-          title={<Typography variant="h4">FOOD BOX DELIVERY</Typography>}
-          subheader={mission.details}
-        />
+        <CardActionArea onClick={toggle}>
+          <CardHeader
+            className={classes.cardHeader}
+            avatar={<AppleIcon className={classes.appleIcon} />}
+            title={<Typography variant="h4">FOOD BOX DELIVERY</Typography>}
+            subheader={mission.details?.map((r) => (
+              <Typography key={r.resourceUid} variant="subtitle2">
+                {r.quantity} x {r.displayName}
+              </Typography>
+            ))}
+          />
+        </CardActionArea>
+        <Collapse in={open}>
+          <Details mission={mission} />
+        </Collapse>
+        <CardActions className={classes.cardActions}>
+          <Button onClick={toggle} color="primary" size="small">
+            {open ? "HIDE DETAILS" : "MORE DETAILS"}
+          </Button>
+        </CardActions>
       </Card>
     </>
   );
@@ -50,6 +92,18 @@ type TopBarProps = {
   createdDate: string;
   className?: string;
 };
+
+function Icon({ status, ...rest }: any) {
+  return status === Status.started ? (
+    <PlayCircleFilled {...rest} />
+  ) : status === Status.delivered ? (
+    <NotificationImportant {...rest} />
+  ) : status === Status.succeeded ? (
+    <CheckCircle {...rest} />
+  ) : (
+    <FiberNew {...rest} />
+  );
+}
 
 function _TopBar({ className, createdDate, status }: TopBarProps) {
   const date = new Date(createdDate).toLocaleDateString();
@@ -96,14 +150,47 @@ const TopBar = styled(_TopBar)`
   }
 `;
 
-function Icon({ status, ...rest }: any) {
-  return status === Status.started ? (
-    <PlayCircleFilled {...rest} />
-  ) : status === Status.delivered ? (
-    <NotificationImportant {...rest} />
-  ) : status === Status.succeeded ? (
-    <CheckCircle {...rest} />
-  ) : (
-    <FiberNew {...rest} />
+const EditLink = styled(Button)`
+  background: none;
+  border: none;
+  padding: 0;
+  color: ${({ theme }) => theme.palette.primary.main};
+  text-decoration: underline;
+  font-weight: 300;
+`;
+
+function Details({ mission }: { mission: MissionInterface }) {
+  const org = useOrganization();
+  return (
+    <>
+      <Divider />
+      <Box margin="1rem 2rem">
+        <Typography variant="h5" color="textPrimary">
+          Address
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {mission.deliveryLocation.address}
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h5" color="textPrimary">
+            Drop-off instructions
+          </Typography>
+          <EditLink onClick={() => {}}>Edit</EditLink>
+        </Box>
+        <Typography variant="body1">{mission.deliveryNotes}</Typography>
+        {/* <Typography variant="h5" color="textPrimary">
+          Total Cost
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {mission.deliveryLocation.address}
+        </Typography> */}
+        {/* <Typography variant="h5" color="textPrimary">
+          Need Help?
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Contact you organizer at {org.phoneNumber}
+        </Typography> */}
+      </Box>
+    </>
   );
 }
