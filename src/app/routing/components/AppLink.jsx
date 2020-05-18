39f21b171882de37d31dbcac663eabdb5375755a
Amy Lo <amyamy86@gmail.com>
2020-05-18 00:07:51 -0700

    feat: [489] Make AppLink component respect user role permissions

--- src/app/routing/components/AppLink.jsx
+++ src/app/routing/components/AppLink.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { RoutingService } from "../index";
+import { useSelector } from "react-redux";
+import { UserPermissionsService } from "../../model/permissions";
+import { isLoaded } from "react-redux-firebase";
 
 function AppLink({ children, to, ...rest }) {
-  console.log("APPLINK to:", to);
-  return (
-    <Link to={to} {...rest}>
-      {children}
-    </Link>
-  );
+  const auth = useSelector((state) => state.firebase.auth);
+  const [canAccess, setCanAccess] = useState(false);
+
+  useEffect(() => {
+    if (isLoaded(auth) && UserPermissionsService.role) {
+      const routeAccess = RoutingService.canAccessRoute(to);
+      setCanAccess(routeAccess.permissionGranted);
+    }
+  }, [auth, to]);
+
+  if (canAccess) {
+    return (
+      <Link to={to} {...rest}>
+        {children}
+      </Link>
+    );
+  } else {
+    return null;
+  }
 }
 
 AppLink.propTypes = {
