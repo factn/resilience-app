94f4d80a7b28483af53ff442153df4b14d1ed77d
Amy Lo <amyamy86@gmail.com>
2020-05-18 19:48:41 -0700

    fix: [576] Fix unit tests

--- src/app/page/Home/Home.js
+++ src/app/page/Home/Home.js
@@ -394,7 +394,7 @@ const HomePage = ({ auth, history, profile }) => {
   return (
     <Page isLoaded={isLoaded(auth)} LoadingComponent={LoadingComponent}>
       {isLoaded(auth) && !isEmpty(auth) ? (
-        <AuthenticatedHome />
+        <AuthenticatedHome auth={auth} />
       ) : (
         <Grid container>
           <SignInHeaderComponent history={history} />
