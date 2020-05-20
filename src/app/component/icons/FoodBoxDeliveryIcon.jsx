da59eddb10d81a69f780e0d277e3d26f340d35ab
Taylor <wwwaaavvveeerrr@gmail.com>
2020-05-20 02:57:48 -0400

    changed route to take you directly to the food box creation screen instead of start

--- src/app/component/icons/FoodBoxDeliveryIcon.jsx
+++ src/app/component/icons/FoodBoxDeliveryIcon.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as BoxIcon } from "../../../img/food_box_delivery.svg";
 import { iconStylesWhite } from "./styles";
 
 const FoodBoxDeliveryIcon = (props) => {
-  return <BoxIcon className="food-box-icon" style={iconStylesWhite} {...props} />;
+  return <BoxIcon style={iconStylesWhite} {...props} />;
 };
 
 export default FoodBoxDeliveryIcon;
